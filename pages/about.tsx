b72e97f9278702f1514a8fe69adaee7d824c5a56
isharaman8 <isharaman8@gmail.com>
2022-04-26 10:59:06 +0530

    Created basic structure and navbar

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+function About() {
+	return (
+		<>
+			<Button variant="contained">Click</Button>
+		</>
+	);
+}
+
+export default About;
