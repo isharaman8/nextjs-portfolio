1ef87ebc3bc49c9ea8122bac1718d1095a1866de
Aman Kumar <isharaman8@gmail.com>
2022-08-01 23:53:09 +0530

    added threejs pixel pc model and implemented dark theme

---
+++ lib/model.ts
@@ -0,0 +1,41 @@
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+export function loadGLTFModel(
+    scene: any,
+    glbPath: any,
+    options = { receiveShadow: true, castShadow: true }
+) {
+    const { receiveShadow, castShadow } = options
+    return new Promise((resolve, reject) => {
+        const loader = new GLTFLoader()
+
+        loader.load(
+            glbPath,
+            (gltf) => {
+                const obj = gltf.scene
+                obj.name = 'dog'
+                // obj.position.setX(-100)
+                obj.position.y = 0
+                obj.position.x = 0
+                obj.receiveShadow = receiveShadow
+                obj.castShadow = castShadow
+                obj.scale.x = 100
+                obj.scale.y = 100
+                obj.scale.z = 100
+                scene.add(obj)
+
+                obj.traverse(function (child: any) {
+                    if (child.isMesh) {
+                        child.castShadow = castShadow
+                        child.receiveShadow = receiveShadow
+                    }
+                })
+                resolve(obj)
+            },
+            undefined,
+            function (error) {
+                reject(error)
+            }
+        )
+    })
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,102 +1,134 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import Navbar from "../components/Navbar";
-import Particles from "react-tsparticles"
-import {loadFull} from "tsparticles"
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import Navbar from '../components/Navbar'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { useTheme } from '@emotion/react'
+import * as React from 'react'
+import { Button } from '@mui/material'
+import ColorModeContext from '../components/ColorModeContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const theme = useTheme()
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light')
+    const particlesInit = async (main: any) => {
+        console.log(main)
+        await loadFull(main)
+    }
+    const particlesLoaded = (container: any): any => {
+        console.log(container)
+    }
+    const darkTheme = createTheme({
+        palette: {
+            mode,
+        },
+    })
 
-   const particlesInit = async (main: any) => {
-      console.log(main);
-      await loadFull(main);
-   };
-   const particlesLoaded = (container: any):any => {
-      console.log(container);	
-   }
-   return (
-      <>
-         <Navbar />
-         <Particles
-            id="tsparticles"
-            init={particlesInit}
-            loaded={particlesLoaded}
-            options={{
-               background: {
-                  opacity: 0,
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'))
+            },
+        }),
+        [mode]
+    )
 
-               },
-                  fpsLimit: 120,
-                  interactivity: {
-                     events: {
-                        onClick: {
-                           enable: true,
-                              mode: "push",
-                        },
-                           onHover: {
-                              enable: true,
+    return (
+        <>
+            <ColorModeContext.Provider value={colorMode}>
+                <ThemeProvider theme={darkTheme}>
+                    <CssBaseline />
+                    <Navbar />
+                    <Particles
+                        id="tsparticles"
+                        init={particlesInit}
+                        loaded={particlesLoaded}
+                        options={{
+                            background: {
+                                opacity: 0,
+                            },
+                            fpsLimit: 120,
+                            interactivity: {
+                                events: {
+                                    onClick: {
+                                        enable: true,
+                                        mode: 'push',
+                                    },
+                                    onHover: {
+                                        enable: true,
 
-                                 mode: "repulse",
-                           },
-                           resize: true,
-                     },
-                        modes: {
-                           push: {
-                              quantity: 4,
-                           },
-                              repulse: {
-                                 distance: 200,
-                                    duration: 0.4,
-                              },
-                        },
-                  },
-                  particles: {
-                     color: {
-                        value: "#5c7aff",
-                     },
-                        links: {
-                           color: "#5c7aff",
-                              distance: 150,
-                              enable: true,
-                              opacity:0.4,
-                              width: 1,
-                        },
+                                        mode: 'repulse',
+                                    },
+                                    resize: true,
+                                },
+                                modes: {
+                                    push: {
+                                        quantity: 0,
+                                    },
+                                    repulse: {
+                                        distance: 200,
+                                        duration: 0.4,
+                                    },
+                                },
+                            },
+                            particles: {
+                                color: {
+                                    value:
+                                        mode === 'light'
+                                            ? '#5c7aff'
+                                            : '#ffffff',
+                                },
+                                links: {
+                                    color:
+                                        mode === 'light'
+                                            ? '#5c7aff'
+                                            : '#ffffff',
+                                    distance: 150,
+                                    enable: true,
+                                    opacity: 0.4,
+                                    width: 0.5,
+                                },
 
-                        collisions: {
-                           enable: true,
-                        },
-                        move: {
-                           direction: "none",
-                              enable: true,
-                              outModes: {
-                                 default: "bounce",
-                              },
-                              random: false,
-                              speed: 1,
-                              straight: false,
-                        },
-                        number: {
-                           density: {
-                              enable: true,
-                                 area: 1000,
-                           },
-                              value: 80,
-                        },
-                        opacity: {
-                           value: 0.5
-                        },
-                        shape: {
-                           type: "circle",
-                        },
-                        size: {
-                           value: { min: 1, max: 5 },
-                        },
-                  },
-                  detectRetina: false,
-            }}
-         />
-         <Component {...pageProps} />;
-      </>
-   );
+                                collisions: {
+                                    enable: true,
+                                },
+                                move: {
+                                    direction: 'none',
+                                    enable: true,
+                                    outModes: {
+                                        default: 'bounce',
+                                    },
+                                    random: false,
+                                    speed: 1,
+                                    straight: false,
+                                },
+                                number: {
+                                    density: {
+                                        enable: true,
+                                        area: 2000,
+                                    },
+                                    value: 80,
+                                },
+                                opacity: {
+                                    value: 0.5,
+                                },
+                                shape: {
+                                    type: 'square',
+                                },
+                                size: {
+                                    value: { min: 1, max: 5 },
+                                },
+                            },
+                            detectRetina: false,
+                        }}
+                    />
+                    <Component {...pageProps} />;
+                </ThemeProvider>
+            </ColorModeContext.Provider>
+        </>
+    )
 }
 
-export default MyApp;
+export default MyApp
